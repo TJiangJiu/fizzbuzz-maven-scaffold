c8a5c2f9e565c3480edab084eef4b63ed2fdf5e3
niuerge <T_JiangLiu@163.com>
2019-12-28 11:49:08 +0800

    pass java_江柳_成都项目建设处

--- src/main/java/PrintNumberGame.java
+++ src/main/java/PrintNumberGame.java
@@ -1,5 +1,13 @@
 public class PrintNumberGame {
-    public static String printNumber(int number) {
-        return null;
+    public static String printNumber(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(i);
+        }
     }
 }
